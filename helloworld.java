6aa1d537fd0748a25ef6a5742c90dcf3d93c5cfa
suyogmetange <110580455+suyogmetange@users.noreply.github.com>
2022-08-23 01:12:21 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,5 @@
+public class helloworld {
+   public static void main(String[] args) {
+    System.out.println("Hello World - 18th August 2022 - Afternoon 02:26 PM - triggered by webhook");
+    }
+}
