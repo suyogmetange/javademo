d076bb013fad45c48084295dd65ce3e2dd2bf629
suyogmetange <110580455+suyogmetange@users.noreply.github.com>
2022-08-27 12:28:34 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World updates initiated completed");
+    System.out.println("Hello World updates completed");
     }
 }
